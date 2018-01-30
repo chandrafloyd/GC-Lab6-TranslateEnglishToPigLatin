de40b0792bf79838bcc8f92aa4f766c50f12b6d1
chandrafloyd <chandra_floyd@yahoo.com>
2018-01-30 15:33:40 -0500

    Corrected the IndexOfAny and the methods

--- Lab6-TranslateEnglishToPigLatin/Program.cs
+++ Lab6-TranslateEnglishToPigLatin/Program.cs
@@ -20,23 +20,24 @@ namespace Lab6_TranslateEnglishToPigLatin
                 //INPUT AND OTHER VARIABLES
 
                 string OriginalWord = Console.ReadLine();   //add validation: if upper or title case, convert to lower case
-                string FirstVowel;
+                string WordEnd, WordBeginning;
                 char[] Vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
 
-                //PROCESS - Consonant or vowel?
-
+                
                 //FIND THE FIRST VOWEL - NTS: is vowel in the first position or no? if first position, FirstVowel = true; add +way and end game
 
-                FirstVowel = Convert.ToChar(OriginalWord.IndexOf(Vowels));  //NTS: how to make it pull from the array?
+                int index = OriginalWord.IndexOfAny(Vowels);  //this finds the index of the first occurence of the vowel. ie, it looks at OriginalWord, compares it ot the list of vowels and finds the index of the first one and sets the value to an int. In this case, the int is called index.
 
-                Console.WriteLine("The first vowel in " + OriginalWord + " is found at position {1}",  //CWL to validate finding correct letter; remove later
+                WordEnd = OriginalWord.Substring(index); //uses the found index to cut the string into a substring. The cutoff point starts at index and goes to the end of the string.
+                WordBeginning = OriginalWord.Substring(0, index);  //this starts at the first position and reads up to the first occurence of the index
 
-                    OriginalWord, OriginalWord.IndexOfAny(Vowels) + 1);
 
-                if (FirstVowel == 0)
+
+                
+                if (index == 0)
                 {
-                    Console.WriteLine("Translation: " + OriginalWord.ToLower() + "way");
+                    Console.WriteLine("Translation: " + OriginalWord + "way");
                     Console.WriteLine(" ");
                     Console.WriteLine("You are now bilingual!");
                     Console.WriteLine(" ");
@@ -44,19 +45,16 @@ namespace Lab6_TranslateEnglishToPigLatin
 
                 else  //First letter is a consonant
                 {
-                    //NTS: insert the code that will loop through, remove the first consonant, move it to end of word, add "ay" (word becomes ord+w+ay)
                     //take all letters in front of the FirstVowel and append to end of the word
 
-
+                    Console.WriteLine($"Original = {OriginalWord} new word = {WordEnd}{WordBeginning}ay");
                 }
 
-            }
-            
-            //PLAY OR END GAME?
+                //PLAY OR END GAME?
                 Console.WriteLine("Play again? Y/N");
                 string Continue = Console.ReadLine();
 
-                if (Continue == "N")   //if continue = N, End the game
+                if (Continue.ToLower() == "n" || Continue.ToLower() == "no")   //if continue = N, End the game
                 {
                     EndGame = true;  //set EndGame to true in order to end the loop and exit
                     Console.WriteLine("Thanks for playing!");
@@ -69,70 +67,73 @@ namespace Lab6_TranslateEnglishToPigLatin
 
                 }
 
+            }
+
+
 
 
             ////Example
-                //// The input string
-                //const string value = "Your dog is cute.";
-
-                //// Test with IndexOf method.
-                //if (value.IndexOf("dog") != -1)
-                //    {
-                //        Console.WriteLine("string contains dog!");
-                //    }
-
-
+            // The input string
+            //string value = "Your dog is cute.";
+            //int temp = value.IndexOf("o");
+            //// Test with IndexOf method
+            //if (temp != -1)
+            //{
+            //    Console.WriteLine(temp);
+            //}
 
 
 
-            //Example
-                //string pigLatin = "";
-                //string firstLetter;
-                //string restOfWord;
-                //string vowels = "AEIOUaeiou";
-                //int letterPosition;
-
-                //foreach (string word in OriginalWord.Split())
-                //{
-                //    firstLetter = word.Substring(0, 1);
-                //    restOfWord = word.Substring(1, word.Length - 1);
-                //    letterPosition = vowels.IndexOf(firstLetter);
-                //        if (letterPosition == -1)
-                //        {
-                //            pigLatin = restOfWord + firstLetter + "ay";
-                //        }
-                //        else
-                //        {
-                //            pigLatin = word + "way";
-                //        }
-                //txtTranslation.Text = pigLatin;
-                //txtInput.Focus();
 
 
             //Example
-                //FirstLetter = (OriginalWord.IndexOf("a"));
+            //string pigLatin = "";
+            //string firstLetter;
+            //string restOfWord;
+            //string vowels = "AEIOUaeiou";
+            //int letterPosition;
 
-                //Console.WriteLine("The first vowel in " + OriginalWord + " is found at position {1}",
-                //                      OriginalWord, OriginalWord.IndexOfAny(Vowels) + 1);
+            //foreach (string word in OriginalWord.Split())
+            //{
+            //    firstLetter = word.Substring(0, 1);
+            //    restOfWord = word.Substring(1, word.Length - 1);
+            //    letterPosition = vowels.IndexOf(firstLetter);
+            //        if (letterPosition == -1)
+            //        {
+            //            pigLatin = restOfWord + firstLetter + "ay";
+            //        }
+            //        else
+            //        {
+            //            pigLatin = word + "way";
+            //        }
+            //txtTranslation.Text = pigLatin;
+            //txtInput.Focus();
 
-                ////Example
-                //string str = "This";
-                //string[] output = str.Split(' ');
-                //foreach (string s in output)
-                //{
-                //    Console.Write(s[0] + " ");
-                //}
 
-                // Output
-                // T i m s
+            //Example
+            //FirstLetter = (OriginalWord.IndexOf("a"));
+
+            //Console.WriteLine("The first vowel in " + OriginalWord + " is found at position {1}",
+            //                      OriginalWord, OriginalWord.IndexOfAny(Vowels) + 1);
+
+            ////Example
+            //string str = "This";
+            //string[] output = str.Split(' ');
+            //foreach (string s in output)
+            //{
+            //    Console.Write(s[0] + " ");
+            //}
+
+            // Output
+            // T i m s
 
 
 
             ////EXAMPLE
-                //String s = "long and winding road...";
-                //Console.WriteLine(" ");
-                //Console.WriteLine("The first vowel in \n {0}\n is found at position {1}",
-                //                  s, s.IndexOfAny(Vowels) + 1);
+            //String s = "long and winding road...";
+            //Console.WriteLine(" ");
+            //Console.WriteLine("The first vowel in \n {0}\n is found at position {1}",
+            //                  s, s.IndexOfAny(Vowels) + 1);
 
 
 
